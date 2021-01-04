ac4a82b19ebfc2f59cfd7ddddf32d2ddb80ddfe2
Snnoky <robirobipl@gmail.com>
2021-01-05 00:59:54 +0100

    Testing TODO

--- psiDjango/quickstart/models.py
+++ psiDjango/quickstart/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+# TODO
+
+# Video
+# title
+# description
+# path
+# date tiem
+# user ( posted by user )
+
+#Comment
+# text
+# user ( posted by user )
+# datetime
