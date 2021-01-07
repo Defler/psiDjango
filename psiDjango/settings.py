510e7f4e2e2ac842b25beefdbdcee0a496edbe22
Snnoky <robirobipl@gmail.com>
2021-01-07 13:34:20 +0100

    settings update

--- psiDjango/settings.py
+++ psiDjango/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "psibaza",
         'USER': "root",
-        'PASSWORD': "",
+        'PASSWORD': "pwdpwd",
         'HOST': "localhost",
         'PORT': "3306",
     }
